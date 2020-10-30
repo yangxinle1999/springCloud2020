7eaea4d3228bf8dfa7459e173a03312c113d67db
yangxinle1999 <2842611637@qq.com>
2020-10-30 14:12:29 +0800

    springCloud2020基础代码

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entity/user.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class user implements Serializable {
+    private int id;
+    private String name;
+    private int age;
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/paymentMain8001.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class paymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(paymentMain8001.class,args);
+    }
+}
